4933874307aa6a7661e67ce9f513f4a2856fae78
Barack Obama <barack.obama@gmail.com>
2018-04-28 23:20:09 -0700

    Initial commit

---
+++ src/test/java/core/CalcTest.java
@@ -0,0 +1,29 @@
+package core;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CalcTest {
+
+	@Test
+	public void test_01() {
+		System.out.println("Test # 01");
+	}
+	
+	@Test
+	public void test_02() {
+		System.out.println("Test # 02");
+	}
+	
+	@Test
+	public void test_03() {
+		System.out.println("Test # 03");
+	}
+	
+	@Test
+	public void test_04() {
+		System.out.println("Test # 04");
+	}
+
+}
